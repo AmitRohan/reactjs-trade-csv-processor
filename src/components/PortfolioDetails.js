c5e8dbd412c886c5642e23f4987a1db2381a35d1
Amit Rohan <a17rohan@gmail.com>
2021-11-23 03:42:32 +0530

    Improved link with parent

---
+++ src/components/PortfolioDetails.js
@@ -0,0 +1,86 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import CurrentCoinBalance from './CurrentCoinBalance';
+
+const defaultCoinObject = {
+  coinsOwned : 0,
+  currentValue : 0,
+  fee : 0,
+  moneyInvested : 0,
+  moneyInvestedWithFees : 0
+};
+
+
+const Tab = styled.button`
+  font-size: 20px;
+  padding: 10px 60px;
+  cursor: pointer;
+  opacity: 0.6;
+  background: white;
+  border: 0;
+  outline: 0;
+  ${({ active }) =>
+    active &&
+    `
+    border-bottom: 2px solid black;
+    opacity: 1;
+  `}
+`;
+const ButtonGroup = styled.div`
+  display: flex;
+`;
+
+class PortfolioDetails extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      active : "",
+      selectedCoinData : defaultCoinObject,
+      selectedCoinDataSet : [],
+      selectedCoinPrice : -1,
+    } 
+  }
+  getTabs = () =>  {
+    var allSuportedCoins = this.props.allSuportedCoins
+    return allSuportedCoins.map( coin => {
+      return (<Tab
+                key={coin}
+                active={this.state.active === coin}
+                onClick={() => { 
+                  if(this.state.active === coin){
+                    return;
+                  }
+                  this.props.updateSelectedToken(coin);
+                  this.setState({ active : coin})
+                }}
+              >
+                {coin}
+              </Tab>)
+    });
+  }
+
+  render(){
+    return (
+      <div>
+        <h4>Coin Details</h4>
+        <ButtonGroup>
+          {
+            this.getTabs()
+          }
+        </ButtonGroup>
+        {
+            this.props.selectedCoinData !== defaultCoinObject
+              ? <div> </div>
+              : (<CurrentCoinBalance
+                  coinToken = {this.props.selectedCoinToken}
+                  coinPrice = {this.props.selectedCoinPrice}
+                  coinData = {this.props.selectedCoinData}
+                />)
+        }
+      </div>
+    );
+  }
+}
+
+export default PortfolioDetails;
