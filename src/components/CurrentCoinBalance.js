1c5a6d54749726c244ac786d410493d89781de13
amitrohan <amit@kruzr.co>
2021-11-27 01:43:07 +0530

    Updated UI

--- src/components/CurrentCoinBalance.js
+++ src/components/CurrentCoinBalance.js
@@ -1,48 +1,55 @@
+import { Card, CardContent, Typography } from '@mui/material';
 import React, { Component } from 'react';
 
 class CurrentCoinBalance extends Component {
   
-
-  constructor(props) {
-    super(props);
-    
-  }
-  
   render(){
     return (
-      <div>
-        <header>
-            <div>
-              <h5>Coins Token</h5>
-              <p>{this.props.coinToken}</p>
-            </div>
-            <div>
-              <h5>Coins Owned</h5>
-              <p>{this.props.coinData.coinsOwned}</p>
-            </div>
-            <div>
-              <h5>Coin Price</h5>
-              <p>{this.props.coinPrice}</p>
-            </div>
-            
-            <div>
-              <h5>Current Value</h5>
-              <p>{this.props.coinData.currentValue}</p>
-            </div>
-            <div>
-              <h5>Fees Paid</h5>
-              <p>{this.props.coinData.fee}</p>
-            </div>
-            <div>
-              <h5>Money Invested (Without fee)</h5>
-              <p>{this.props.coinData.moneyInvested}</p>
-            </div>
-            <div>
-              <h5>Money Invested (With fee)</h5>
-              <p>{this.props.coinData.moneyInvestedWithFees}</p>
-            </div>
-        </header>
-      </div>
+      <Card sx={{ minWidth: 275 }}>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {this.props.coinData.coinsOwned + " " + this.props.coinToken + " Owned"} 
+          </Typography>
+          <Typography gutterBottom variant="h6" component="div">
+            Price
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {this.props.coinPrice}
+          </Typography>
+
+          <Typography gutterBottom variant="h6" component="div">
+            Current Value
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {this.props.coinData.currentValue}
+          </Typography>
+
+          <Typography gutterBottom variant="h6" component="div">
+            Money Invested (Without fee)
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {this.props.coinData.moneyInvested}
+          </Typography>
+
+
+          <Typography gutterBottom variant="h6" component="div">
+            Fees Paid
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+          {this.props.coinData.fee}
+          </Typography>
+
+
+          <Typography gutterBottom variant="h6" component="div">
+            Money Invested (With fee)
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {this.props.coinData.moneyInvestedWithFees}
+          </Typography>
+
+
+        </CardContent>
+      </Card>
     );
   }
 }
