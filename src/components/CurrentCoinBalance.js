8c50daaa72faf446dfc19cee8bf078c7716d0e7d
amitrohan <amit@kruzr.co>
2021-11-28 00:11:16 +0530

    Integrated apexcharts

--- src/components/CurrentCoinBalance.js
+++ src/components/CurrentCoinBalance.js
@@ -1,8 +1,55 @@
 import { Card, CardContent, Typography } from '@mui/material';
 import React, { Component } from 'react';
+import ReactApexChart from "react-apexcharts";
+
+
+const chartConfig = {
+  series: [],
+  options: {
+    chart: {
+      height: 350,
+      type: 'line',
+      zoom: {
+        enabled: false
+      }
+    },
+    dataLabels: {
+      enabled: false
+    },
+    stroke: {
+      curve: 'straight'
+    },
+    title: {
+      text: 'Last 90 price',
+      align: 'left'
+    },
+    grid: {
+      row: {
+        colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+        opacity: 0.5
+      },
+    },
+    xaxis: {
+      categories: [],
+    }
+  },
+};
 
 class CurrentCoinBalance extends Component {
   
+  getGraph = () => {
+    const coinHistoricPrice = this.props.coinHistoricPrice || []
+    const options = Object.assign({},chartConfig.options)
+    options.xaxis.categories= coinHistoricPrice.map( x => new Date(x[0]).toLocaleDateString())
+    const series = [{
+      name: this.props.coinToken,
+      data: coinHistoricPrice.map( x => x[1])
+  }]
+    return (
+      <ReactApexChart options={options} series={series} type="line" height={350} />
+    )
+  }
+
   render(){
     return (
       <Card sx={{ minWidth: 275 }}>
@@ -47,6 +94,9 @@ class CurrentCoinBalance extends Component {
             {this.props.coinData.moneyInvestedWithFees}
           </Typography>
 
+          {
+            this.getGraph()
+          }
 
         </CardContent>
       </Card>
