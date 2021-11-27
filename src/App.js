c122c367234369f073308e4b0d5010a12fe4373b
amitrohan <amit@kruzr.co>
2021-11-27 08:13:28 +0530

    Cleanup

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import ReactFileReader from 'react-file-reader';
 import React, { Component } from 'react';
-import CurrentCoinBalance from './components/CurrentCoinBalance';
 import PortfolioOverview from './components/PortfolioOverview';
 import PortfolioDetails from './components/PortfolioDetails';
 import { AppBar, Button, IconButton, Toolbar, Typography } from '@mui/material';
@@ -46,7 +45,7 @@ class App extends Component {
     // Set Checkpoint Size
     this.postProcessingCheckpointCounter = 0;
     var postProcessingCheckpoints = 999999;
-    this.setState({ postProcessingCheckpoints , postProcessingDone : true});
+    this.setState({ postProcessingCheckpoints , postProcessingDone : false});
 
     var reader = new FileReader();
     reader.onload = (e) => {
@@ -59,10 +58,10 @@ class App extends Component {
                         this.getAllCoinsFromReport(fileData)
                           .filter( coin =>{
                               // return ( -1 != ["BTC","ETH","DOGE"].indexOf(coin))
-                              return ( -1 != ["BTC","ETH"].indexOf(coin))
+                              return ( -1 !== ["BTC","ETH"].indexOf(coin))
                           });
           this.setState( { allSuportedCoins , fileData })
-          // this.updateLatestCoinPricesFromCoinGecko();
+          this.updateLatestCoinPricesFromCoinGecko();
 
         }) 
     }
@@ -125,7 +124,7 @@ class App extends Component {
 
 
           cb();
-      });
+      }).catch(err => console.log(err));
   }
 
   updateLatestCoinPricesFromCoinGecko = () => {
@@ -137,7 +136,7 @@ class App extends Component {
                 return;
             }
             var suportedResponse = resp.data.filter( coinResp => {
-              return (-1 != this.state.allSuportedCoins.indexOf(coinResp.symbol.toUpperCase()))
+              return (-1 !== this.state.allSuportedCoins.indexOf(coinResp.symbol.toUpperCase()))
             });
             // Set Checkpoint Size
             this.postProcessingCheckpointCounter = 0;
@@ -145,7 +144,7 @@ class App extends Component {
             this.setState({ postProcessingCheckpoints });
             
             var allCoinCoinGeckoId = this.state.allCoinCoinGeckoId;
-            suportedResponse.map( coinResp => {
+            suportedResponse.forEach( coinResp => {
                 var coinSymbol = coinResp.symbol.toLowerCase();
                 var indexInSuportedCoins = this.state.allSuportedCoins.indexOf(coinSymbol.toUpperCase())
                 allCoinCoinGeckoId[indexInSuportedCoins] = coinResp.id
@@ -168,7 +167,7 @@ class App extends Component {
     })
   }
 
-  fetchLatestDataFromCoinGecko = () => {
+  fetchSelectedCoinIdFromCoinGecko = () => {
     CoinGeckoClient
         .coins
         .list()
@@ -176,34 +175,42 @@ class App extends Component {
             if(resp.code !== 200){
                 return;
             }
-            resp.data.map( coinResp => {
+            resp.data.forEach( coinResp => {
                 var coinSymbol = coinResp.symbol.toLowerCase();
                 if(coinSymbol === this.state.selectedCoinToken.toLowerCase()){
                     
                     // Get Data
-                    console.log("Fetching Latest Coin Data");
-                    CoinGeckoClient.coins.fetch(coinResp.id, {})
-                        .then(coinDataReponse => {
-                            console.log("Latest Coin Data Fetched");
-                            var selectedCoinName = this.state.selectedCoinToken;
-                            // const selectedCoinPrice = 1
-                            const selectedCoinPrice = coinDataReponse.data.market_data.current_price.inr
-                            this.updateSelectedCoinInState(selectedCoinPrice);
-                        });
-                    console.log("Fetching Historic Prices");
-                    CoinGeckoClient.coins.fetchMarketChart(coinResp.id, {days : 91, vs_currency : 'inr' , interval : 'daily '})
-                      .then(coinMarketChartData => {
-                          console.log("Historic Prices Fetched");
-                          const historicPrices = coinMarketChartData
-                                                      .data
-                                                      .prices
-                                                      .filter((x,i)=> i > 61) // save last 5 entry as interval field is not supported yet
-                                                      .map(x => { return Math.round(x[1]*10)/10}); // get abs value of price, 2nd param, 1st is timestamp
-                          
-                      });
+                    this.fetchCoinPrice(coinResp.id)
+                    
+                    this.fetchHistoricPrices(coinResp.id)
             }
         })
-    })
+    }).catch(err => console.log(err));
+  }
+
+  fetchCoinPrice = (coinId) => {
+    CoinGeckoClient
+      .coins
+      .fetch(coinId, {})
+      .then(coinDataReponse => {
+          // const selectedCoinPrice = 1
+          const selectedCoinPrice = coinDataReponse.data.market_data.current_price.inr
+          this.updateSelectedCoinInState(selectedCoinPrice);
+      }).catch(err => console.log(err));
+  }
+
+  fetchHistoricPrices = (coinId) => {
+    CoinGeckoClient
+      .coins
+      .fetchMarketChart(coinId, {days : 91, vs_currency : 'inr' , interval : 'daily '})
+      .then(coinMarketChartData => {
+          const historicPrices = coinMarketChartData
+                                      .data
+                                      .prices
+                                      .filter((x,i)=> i > 61) // save last 5 entry as interval field is not supported yet
+                                      .map(x => { return Math.round(x[1]*10)/10}); // get abs value of price, 2nd param, 1st is timestamp
+          
+      }).catch(err => console.log(err));
   }
 
   // UI EVENTS
@@ -224,7 +231,7 @@ class App extends Component {
 
   handleNewTokenSelection = (selectedCoinToken) => {
     this.setState( { selectedCoinToken })
-    this.fetchLatestDataFromCoinGecko();
+    this.fetchSelectedCoinIdFromCoinGecko();
   }
 
   render(){
@@ -262,10 +269,10 @@ class App extends Component {
                 </header> 
               : 
               <div>
-                {/* <PortfolioOverview
+                <PortfolioOverview
                   allCoinData = {this.state.allCoinData}
                   allCoins = {this.state.allSuportedCoins}
-                /> */}
+                />
                 <PortfolioDetails
                   selectedCoinToken = { this.state.selectedCoinToken }
                   selectedCoinPrice = { this.state.selectedCoinPrice}
