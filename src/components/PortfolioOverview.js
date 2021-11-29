eea11a12b30a094a67f9834f102bb461ab35688b
amitrohan <amit@kruzr.co>
2021-11-29 22:24:01 +0530

    Passed coin icon to child

--- src/components/PortfolioOverview.js
+++ src/components/PortfolioOverview.js
@@ -5,11 +5,12 @@ import CoinOverview from './CoinOverview';
 class PortfolioOverview extends Component {
 
   getIndividualCards = () =>  {
-    var allCoinData = this.props.allCoinData
+    var allCoinData = this.props.allCoinData;
     return allCoinData.map( (coin, id) => {
       return (
         <Grid item xs={4} key= { id}>
             <CoinOverview
+              coinIcon = { this.props.allCoinIcon[id]}
               coinToken = { this.props.allCoins[id]}
               coinData = { coin }/>
         </Grid>)
