a3708440ff0777f4f15fd444192dbed5486d606b
amitrohan <amit@kruzr.co>
2021-11-30 17:29:33 +0530

    Cleanup

--- src/components/CoinOverview.js
+++ src/components/CoinOverview.js
@@ -1,7 +1,44 @@
 import { Avatar, Card, CardContent, Typography } from '@mui/material';
 import React, { Component } from 'react';
-import GreenStyledBadge from './GreenStyledBadge';
-import RedStyledBadge from './RedStyledBadge';
+
+// Boiler Plate for Using custom styles for Badge below icon
+import { styled } from '@mui/material/styles';
+import Badge from '@mui/material/Badge';
+
+const GreenStyledBadge = styled(Badge)(({ theme }) => ({
+    '& .MuiBadge-badge': {
+      backgroundColor: '#44b700',
+      color: '#44b700',
+      boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+      '&::after': {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: '100%',
+        height: '100%',
+        content: '""',
+      },
+    }
+}));
+
+
+
+const RedStyledBadge = styled(Badge)(({ theme }) => ({
+  '& .MuiBadge-badge': {
+    backgroundColor: '#b74400',
+    color: '#b74400',
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`,
+    '&::after': {
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      width: '100%',
+      height: '100%',
+      content: '""',
+    },
+  }
+}));
+
 
 class CoinOverview extends Component {
   
